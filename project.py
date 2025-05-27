490294ee382d3cc3cc13e3e8a89e6626b62ffd1d
Kashish <kashish.vaid29994@gmail.com>
2025-05-27 22:50:22 +0530

    learnt query, path, and did hands on

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Path
+from fastapi import FastAPI, HTTPException, Path, Query
 import json
 
 app = FastAPI()
@@ -30,5 +30,20 @@ def view_patient(patient_id: str = Path(..., description= "ID of patient in DB",
     print("Received ID:", repr(patient_id))  
     if patient_id in data:
         return data[patient_id]
-    else:
-        raise HTTPException(status_code=404, detail="Patient does not exist")
+    raise HTTPException(status_code=404, detail="Patient does not exist")
+
+@app.get("/sort")
+def sort_patients(sort_by :str = Query(..., description= "sort on the basis of height, weight or BMI"), order:str = Query("asc",description= "sort in asc or desc order")):
+    valid_fiels = ["height", "weight", 'bmi']
+
+    if sort_by not in valid_fiels:
+        raise HTTPException(status_code= 400, detail=f"invaid field select from {valid_fiels}")
+
+    if order not in ["asc", "desc"]:
+        raise HTTPException(status_code=400, details= "Invalid order select between asc and desc")
+    
+    data = load_data()
+    sort_order = True if order=="desc" else False
+    sorted_data = sorted(data.values(),key= lambda x:x.get(sort_by,0), reverse= sort_order)
+
+    return sorted_data
